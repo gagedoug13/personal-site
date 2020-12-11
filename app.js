ba0733a6578f29bc4869e095be3bad9ac9f05f0f
Gage Thornberry <gagedoug13@gmail.com>
2020-12-10 23:39:41 -0700

    project pictures and icons look good. everything is starting to look great

--- app.js
+++ app.js
@@ -2,16 +2,23 @@
 const addVisualGif = (event) => {
     const baseName = event.src.split('').slice(0, 94).join('')
     event.src = baseName + 'gif'
+    setInterval(() => {
+        event.src = baseName + 'png'
+      
+    },12000)
+    
     
 }
 
 const addBlogGif = (event) => {
     const baseName = event.src.split('').slice(0,83).join('')
-    console.log(baseName)
     event.src = baseName + 'gif'
+    setInterval(() => {
+        event.src = baseName + 'png'
+      
+    },12000)
+
+
 }
-
-
-
 
 // console.log(projectPhoto)
