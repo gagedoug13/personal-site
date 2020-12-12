8f4cc616cd1058ca6735fb5dc60a0cfc2715adac
Gage Thornberry <gagedoug13@gmail.com>
2020-12-12 00:52:25 -0700

    added neighborhood jamz. size looks great

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
 
-const addVisualGif = (event) => {
-    const baseName = event.src.split('').slice(0, 94).join('')
+const addGif = (event) => {
+    const baseName = event.src.split('').slice(0, event.src.length - 3).join('')
+    console.log(baseName)
     event.src = baseName + 'gif'
     setInterval(() => {
         event.src = baseName + 'png'
-      
     },12000)
-    
 }
-
-const addBlogGif = (event) => {
-    const baseName = event.src.split('').slice(0,83).join('')
-    event.src = baseName + 'gif'
-    setInterval(() => {
-        event.src = baseName + 'png'
-      
-    },12000)
-
-
-}
-
-// console.log(projectPhoto)
